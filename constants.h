d4d9ff2b7545d01caebcafa8e356149a37a8c193
Jono <jhi57@uclive.ac.nz>
2017-10-11 12:17:05 +1300

    Added .h files for methods + constants

---
+++ constants.h
@@ -0,0 +1,10 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "pio.h"
+
+const pio_t rows[7];
+
+const pio_t cols[5];
+
+#endif
---
+++ methods.h
@@ -0,0 +1,11 @@
+
+#ifndef HELPER_H
+#define HELPER_H
+
+#include "constants.h"
+
+
+/**Check if the button is pressed*/
+uint8_t button_pressed_p (void)
+
+#endif
